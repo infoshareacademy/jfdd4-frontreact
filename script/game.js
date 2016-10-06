b2b3662846cd76e7a6d99b1e183f094d7eadca75
Patryk Huzarski <phuzarski@gmail.com>
2016-10-06 15:33:33 +0200

    movement mechanism

--- script/game.js
+++ script/game.js
@@ -2,6 +2,31 @@
  * Created by patryk on 10/3/16.
  */
 
+
+// modal window to create the table where the game will be show
+var modal = document.getElementById('myModal');
+
+//button otwierajacy modalne okno
+var btn = document.getElementById("myBtn");
+
+//<span> zamykajacy okienko / sprawdzenie validity 
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    console.log('show game board');
+
+    var isFormValid = $('input.input-email').get(0).checkValidity();
+    if (isFormValid){
+        modal.style.display= "block";
+    }
+};
+
+//gdy uzytkownik kliknie w spana (x) - zamykanie okienka
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+
+
 //deklaclaracja ruszania
 var xpos = 100;
 var xspeed = 1;
@@ -50,29 +75,4 @@ function KeyDown(e) {
     if (code == 39)
         rightPressed = 1;
 }
-
-
-
-
-// modal window to create the table where the game will be show
-var modal = document.getElementById('myModal');
-
-//button otwierajacy modalne okno
-var btn = document.getElementById("myBtn");
-
-//<span> zamykajacy okienko / sprawdzenie validity 
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    console.log('show game board');
-
-    var isFormValid = $('input.input-email').get(0).checkValidity();
-    if (isFormValid){
-        modal.style.display= "block";
-    }
-};
-
-//gdy uzytkownik kliknie w spana (x) - zamykanie okienka
-span.onclick = function () {
-    modal.style.display = "none";
-};
 
